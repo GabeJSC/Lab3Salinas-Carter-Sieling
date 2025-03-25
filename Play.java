abf69f672957f7c6796d9c7944b3a8bb899474e9
Gabriel Salinas-Carter <gjcarter9@gmail.com>
2025-03-25 12:43:04 -0700

    Add files via upload

--- Play.java
+++ Play.java
@@ -11,6 +11,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Play {
+    public static void welcome(){
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Welcome! thank you for choosing our program.");
+        System.out.println("You will be given the choice to play either a guessing game or rock, paper, scissors.");
+        System.out.println("Please choose which ever one you would like and play for as long as you want.");
+        System.out.println("Good luck and have fun!");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
 
     public static void rockPaperScissors() {
         boolean playAgain = true;
